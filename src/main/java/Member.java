5488669bb68729d8608e52364d9dfddbcf954822
RuedigerMoeller <moru0011@gmail.com>
2015-03-15 23:30:38 +0100

    .

---
+++ src/main/java/Member.java
@@ -0,0 +1,11 @@
+import org.nustaq.kollektiv.KollektivMember;
+
+/**
+ * Created by ruedi on 15.03.2015.
+ */
+public class Member {
+
+    public static void main(String a[] ) {
+        KollektivMember.main(a);
+    }
+}
